7cd07bf229259e02349f06b7bd797b50d11a9102
haha <seummd@gmail.com>
2021-03-16 00:29:17 +0600

    final

--- Assignment 2/d1t1.f90
+++ Assignment 2/d1t1.f90
@@ -1,18 +1,27 @@
 program  quadratic
  
     implicit none
-    integer :: a,b,c,dis
+    integer :: a,b,c
+    real ::dis,res1,res2
+    complex :: con
     read(*,*)a,b,c
     dis=(b**2)-(4*a*c)
-    print*,a,b,c,dis
+    
     open(unit=10,file="result.txt")
     if(dis==0)then
         write(10,*)"Roots are real and equal"
+        res1=(-b+sqrt(dis))/(2*a)
+        write(10,"(f10.4)")res1
     else if(dis>0)then
         write(10,*)"Roots are real and different"
+        res1=(-b+sqrt(dis))/(2*a)
+        res2=(-b-sqrt(dis))/(2*a)
+        write(10,"(f10.4,f10.4)")res1,res2
     else
-        write(10,*)"Roots are complex"        
+        write(10,*)"Roots are complex"  
+        con=cmplx((-b/2*a),(sqrt((-1*dis))/2*a))
+        write(10,*)con,conjg(con)      
     end if
-
     
+
 end program quadratic
